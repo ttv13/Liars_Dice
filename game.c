fe69ab32e8e0920cf9fac5bbde1533b127cff190
unknown <v.u.t.thomas@gmail.com>
2024-12-07 13:30:16 -0500

    added roll dice

--- game.c
+++ game.c
@@ -5,6 +5,10 @@
 #define Max_Dice 5
 #define Num_Player 2
 
+//----Function Prototypes----
+void Create_players (player* p);
+void Display_players (player* p);
+void roll_dice (player* p);
 
 //----player Struct for player specific information----
 
@@ -15,31 +19,38 @@ typedef struct player
 
 }player;
 
-
 //----Initialize a player - Iterate through loop to set all players in game----
 
-void Create_players (player* p)  {
+void Create_players (player* player)  {
 
-    p->dice_num = Max_Dice;         //Set player dice number to max dice
+    player->dice_num = Max_Dice;         //Set player dice number to max dice
     for (int i = 0; i < Max_Dice; i++){
-        p->dice[i] = 0;             //Set player dice values to 0 Ready to roll for round 1
+        player->dice[i] = 0;             //Set player dice values to 0 Ready to roll for round 1
     }
 
 }
 
 //----Display player information----
 
-void Display_players (player* p) {
+void Display_players (player* player) {
     
-    printf("Dice num: %d\n  Dice values: ", p->dice_num);       //Print Number of player dice
-    for (int i = 0; i < p->dice_num; i++){
-        printf("%d ", p->dice[i]);                              //Print dice values
+    printf("Dice num: %d\n  Dice values: ", player->dice_num);       //Print Number of player dice
+    for (int i = 0; i < player->dice_num; i++){
+        printf("%d ", player->dice[i]);                              //Print dice values
     }
     printf("\n");
 
 }
 
+//----Roll player dice----
 
+void roll_dice (player* player) {
+    
+    for(int i = 0; i < player->dice_num; i++){
+        player->dice[i] = rand() % 6 + 1;       //Roll player dice from 1-6 
+    }
+
+}
 
 int main ()
 {
